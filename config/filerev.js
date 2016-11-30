429ad8d149efe89715ec8e5782a524f6b8f12539
Quentin <quentin.ordener@web-atrio.com>
2016-11-30 11:18:49 +0100

    resolve builg bug TODO add doc about fonts

--- config/filerev.js
+++ config/filerev.js
@@ -6,8 +6,6 @@
       src: [
         '<%= paths.dist %>/scripts/{,*/}*.js',
         '<%= paths.dist %>/styles/{,*/}*.css',
-        '<%= paths.dist %>/images/{,*/}*.*',
-        '<%= paths.dist %>/styles/fonts/{,*/}*.*',
         '<%= paths.dist %>/*.{ico,png}'
       ]
     }
