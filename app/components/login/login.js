d81acfa4e89e75ce1e90f47d330e0d529d0815c6
Quentin <quentin.ordener@web-atrio.com>
2016-11-30 15:56:09 +0100

    sessionStorage user set

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -9,7 +9,8 @@
 
   angular
           .module('Airbus_Dev_Front.components.login', [
-            'Airbus_Dev_Front.service.users'
+            'Airbus_Dev_Front.service.users',
+            'ngStorage'
           ])
           .component('login', {
             controller: Controller,
@@ -18,9 +19,9 @@
             $canActivate: $canActivate
           });
 
-  Controller.$inject = ['UsersService', '$timeout'];
+  Controller.$inject = ['UsersService', '$timeout', '$sessionStorage', '$rootScope'];
 
-  function Controller(UsersService, $timeout) {
+  function Controller(UsersService, $timeout, $sessionStorage, $rootScope) {
     var ctrl = this;
     ctrl.name = 'login ';
     ctrl.checkUser = function (user, pwd) {
@@ -31,14 +32,23 @@
           $timeout(function () {
             ctrl.isVisible = false;
             $('#loginModal').modal('hide');
-          }, 800);
+          }, 700);
         } else {
           ctrl.wrongUsername = true;
           $timeout(function () {
             ctrl.isVisible = false;
-          }, 800);
+          }, 700);
         }
       });
+    };
+    ctrl.logout = function () {
+      delete $sessionStorage.user;
+      delete $rootScope.user;
+      $rootScope.refresh = true;
+      $timeout(function () {
+        $rootScope.refresh = false;
+        $('#logoutModal').modal('hide');
+      }, 700);
     };
   }
 
